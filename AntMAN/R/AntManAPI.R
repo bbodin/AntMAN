ccb34fb92a051c642de0bb627202a3e18943c1ae
Bruno <bruno.bodin@yale-nus.edu.sg>
2019-06-17 15:34:34 +0800

    Add example

--- AntMAN/R/AntManAPI.R
+++ AntMAN/R/AntManAPI.R
@@ -435,7 +435,27 @@ AM_unipois_mix_hyperparams <- function(alpha0, beta0) {
 #' @param sig02   The sig02 hyperparameter.
 #' @return A configuration list to be used as an argument for mcmc_fit. 
 #' @examples 
-#' AM_uninorm_mix_hyperparams (m0=0,k0=0.1,nu0=1,sig02=1.5)
+#'      
+#'      #### This example ...
+#'      
+#'      data(galaxy)
+#'      y_uvn = galaxy
+#'      mixture_uvn_params = AM_uninorm_mix_hyperparams  (m0=20.83146, k0=0.3333333, nu0=4.222222, sig02=3.661027)
+#'      
+#'      mcmc_params        = AM_mcmc_parameters(niter=20000, burnin=5000, thin=10, verbose=1)
+#'      components_prior   = AM_mix_components_prior_pois (init=3,  a=1, b=1) 
+#'      weights_prior      = AM_mix_weights_prior_gamma(init=2, a=1, b=1)
+#'      
+#'      fit <- AM_mcmc_fit(
+#'        y = y_uvn,
+#'        mix_kernel_hyperparams = mixture_uvn_params,
+#'        mix_components_prior =components_prior,
+#'        mix_weight_prior = weights_prior,
+#'        mcmc_parameters = mcmc_params)
+#'      
+#'      summary (fit)
+#'      plot (fit)
+#'      
 #' @export
 AM_uninorm_mix_hyperparams <- function(m0, k0, nu0, sig02) {
   return ( list ( type = "AM_uninorm_mix_hyperparams", m0 = m0 , k0 = k0 , nu0 = nu0 , sig02 = sig02 ) );
