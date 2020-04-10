925113e0f234af490daa0b2edebaede2e19dc718
Bruno <bruno.bodin@yale-nus.edu.sg>
2020-04-10 09:58:56 +0800

    Add likelihood

--- AntMAN/R/AntManAPI.R
+++ AntMAN/R/AntManAPI.R
@@ -159,7 +159,7 @@ NULL
 #'@export
 AM_plot_similarity_matrix=function(C, ...){
 	#### Co clustering probability : How many time two are in the same custer. 
-
+	
 	n = dim(C)[1]
 	## library(corrplot)
 	## col3 <- colorRampPalette(c("red", "white", "blue")) 
@@ -247,6 +247,48 @@ AM_clustering_estimation_laugreen = function (fit, C = NULL) {
 	return (clust_bind)
 }
 
+#'  This is a diagnostic function used to compute the log likelihood of MCMC results
+#'  
+#'  Given a MCMC output, this function return log likelihood of MCMC results
+#'  
+#'@param fit a \code{\link{AM_mcmc_output}} object with a full output set (using the ALL argument)
+#'  
+#'@return log likelihood of MCMC results
+#'@export
+AM_DIAG_loglikelihood  = function (MCMC_output_AntMAN) {
+	
+	loglike_MCMC_iter = rep(0,niter)
+	
+	for (it in 1:niter) {
+		
+		loglike_MCMC = 0
+		c = MCMC_output_AntMAN$CI[[it]]
+		S_m = MCMC_output_AntMAN$S[[it]]
+		mu_star = MCMC_output_AntMAN$TAU[[it]]$mu
+		Sigma_star = MCMC_output_AntMAN$TAU[[it]]$Sig
+		M = MCMC_output_AntMAN$M[[it]]
+		U = MCMC_output_AntMAN$U[[it]]
+		
+		for (i in 1:N) {
+			loglike_MCMC = loglike_MCMC + log(S_m[c[i]+1]) + dmvnorm(y[i,], mu_star[c[i]+1,], Sigma_star[,,c[i]+1], TRUE);
+		}
+		for (j in 1:M) {
+			loglike_MCMC = loglike_MCMC + dmvnorm(mu_star[j,], mu0, Sigma_star[,,j]/k0, TRUE);
+			loglike_MCMC = loglike_MCMC + log(diwish( Sigma_star[,,j], nu0, Lam0));
+			loglike_MCMC = loglike_MCMC + dgamma(S_m[j], gamma_S, scale=1,log= TRUE);
+		}
+		
+		loglike_MCMC = loglike_MCMC + dgamma(Lambda, a2, scale=1/b2, log=TRUE);
+		loglike_MCMC = loglike_MCMC + dgamma(gamma_S, a1, scale=1/b1,log= TRUE);
+		loglike_MCMC = loglike_MCMC + dgamma(U, N, rate=sum(S_m), log = TRUE);
+		
+		loglike_MCMC_iter[it] = loglike_MCMC
+		
+	}
+	
+	return (loglike_MCMC_iter)
+	
+}
 
 #'  Return maximum likelihood estimation (squared_loss)
 #'  
