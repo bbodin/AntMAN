96e7f68eae579b2558f1aeeb9c5ce7d9263ab279
Bruno <bruno.bodin@yale-nus.edu.sg>
2020-04-18 21:10:30 +0800

    working on AM_extract

--- AntMAN/R/AM_extract.R
+++ AntMAN/R/AM_extract.R
@@ -1,4 +1,3 @@
-
 #######################################################################################
 ###############
 ############### AntMAN Package
@@ -16,31 +15,33 @@ generate_column_names = function(target, r) {
 	return (values);
 }
 
-extract_target = function(fit, target, iterations = NULL){
+
+
+extract_target = function(fit, target, iterations = NULL, debug = FALSE){
 	
 	result = NULL
 	
 	mainpath = strsplit(target, "_")[[1]]
-	## cat("path = ", mainpath, "\n");
+	if (debug) message("path = ", paste(mainpath,collapse = " "), "\n");
 	
 	if (length(mainpath) == 0) {
-		cat("ERROR Invalid variable name(too small)\n",sep = "");
+		warning("ERROR Invalid variable name(too small)\n");
 		return (NULL);
 	}
 	
 	variable_name = mainpath[1];
 	
-	## cat(" - variable_name = ", variable_name, "\n");
+	if (debug) message(" - variable_name = ", variable_name, "\n");
 	
 	if (!is.element(variable_name , names(fit))) {
-		cat("ERROR Invalid variable name '",variable_name, "' has not been found)\n",sep = "");
+		warning("ERROR Invalid variable name '",variable_name, "' has not been found)\n");
 		return (NULL);
 	}
 	
 	# level 1 - get variable
 	variable = fit[[variable_name]];
 	
-	## cat(" - variable taken, class = ",class(variable),"length = ",length(variable),"\n");
+	if (debug) message(" - variable taken, class = ",class(variable),", length = ",length(variable),"\n");
 	
 	explored_iterations = iterations
 	if (is.null(explored_iterations)) {
@@ -60,15 +61,15 @@ extract_target = function(fit, target, iterations = NULL){
 		if (is.numeric(values) || is.integer(values)) {
 			
 			# path can only be a numerical index / we skip
-			## cat("   - This is already numerical, we don't need to go down\n")
+			if (debug) message("   - This is already numerical, we don't need to go down\n")
 			
 		} else  if (is.list(values))  {
 			
 			# path is a list we can access an element
-			## cat("   - This is a list:", names(values), "\n")
+			if (debug) message("   - This is a list:", paste(names(values), collapse = " "), "\n")
 			
 			if (length(path) == 0) {
-				cat("ERROR Invalid variable name(too small)\n",sep = "");
+				warning("ERROR Invalid variable name(too small)\n");
 				return (NULL);
 			} 
 			
@@ -76,89 +77,60 @@ extract_target = function(fit, target, iterations = NULL){
 			path = path[-1];
 			
 			if (!is.element(subitem , names(values))) {
-				cat("ERROR: Cannot find subitem '",subitem,"' among [", paste(names(values), collapse=" "),"]\n",sep = "")
+				warning("ERROR: Cannot find subitem '",subitem,"' among [", paste(names(values), collapse=" "),"]\n")
 				return (NULL);
 			} else {
+				if (debug) message("   - We access subitem: ", subitem, "\n")
 				values = values[[subitem]];
 			}
 			
 		} else {
-			cat("ERROR: UNSUPPORTED TYPE " ,class(values) ," \n",sep = "");
+			warning("ERROR: UNSUPPORTED TYPE " ,class(values) ," \n");
 			return (NULL);
 		}
 		
 		
+		if (is.numeric(values) || is.integer(values)) {
+			if (debug) message("   - lower level can be handle\n");
+		} else {
+			warning("ERROR: UNSUPPORTED TYPE " ,class(values) ," \n");
+			return (NULL);
+		}
+		
 		# now we have values a numerical value or array. 
 		# we flatten it or we get what the index requires.
 		if (length(path) == 0) {
-			## cat("   - We flatten\n");
+			if (debug) message("   - We need to flatten\n");
 			# we flatten 
 		} else {
 			# get the one element selected
-			## cat("   - We get the index\n");
+			if (debug) message("   - We get the index\n");
 			value_index = strtoi(path[1]);
-			## cat("   - value_index = " , value_index ,  "\n");
+			if (debug) message("   - value_index = " , value_index ,  "\n");
 			values = values[value_index];
 		}
+		
+		values = as.vector(unlist(values))
 		
 		if (is.null(result)) {
 			result = data.frame(t(values))
 			names(result) <- generate_column_names(target,c(1:length(values)))
 		} else {
-			result = rbind(result, values);
+			if (ncol(result) > length(values)) {
+				values = c( values , rep(NA, ncol(result) - length(values))); 
+			}
+			
+			while (ncol(result) < length(values)) { ## TODO : Please find more efficient!
+				result = cbind(result,c(NA))
+				names(result) <- generate_column_names(target,c(1:length(values)))
+			}
+			if (ncol(result) != length(values)) {
+				warning("ERROR: NUMBER of COLUMN CHANGED FROM " ,ncol(result) , " to ", length(values)," \n");
+				return (NULL);
+			} else {
+				result = rbind(result, values);
+			}
 		}
 	}
 	return (result)
-}
-
-
-
-#'  Extract values within a AM_mcmc_output object. 
-#' 
-#'  Due to the complexity of AntMAN outputs, AM_mcmc_output object can be difficult
-#'  to handle. The AM_extract function ease access of particular variables within 
-#'  AM_mcmc_output object.
-#'  
-#'@param object a \code{\link{AM_mcmc_output}} object
-#'@param targets List of variables to extract (ie. K, M, Mna, Tau_mu).
-#'@param iterations Can specify particular iterations to extracts, NULL for all.
-#'  
-#'@export
-AM_extract = function(object, targets, iterations = NULL){
-	
-	df = NULL;
-	for (target in targets) {
-		
-		
-		if (target == "CI") {
-			## CI Extractor
-			nrows = length(object$CI)
-			ncols = length(object$CI[[1]])
-			tmp = data.frame(array(as.numeric(unlist(object$CI)), dim=c(nrows, ncols)))
-			names(tmp) <- generate_column_names(target,c(1:ncols));
-			if (!is.null(iterations)) {
-				tmp = tmp[iterations,];
-			}
-		} else {
-			## Generic extractor (SLOW)
-			tmp = extract_target(object,target,iterations);
-		}
-		
-		
-		if (is.null(tmp)) {
-			cat("ERROR: Invalid extraction target: ",target,", please make sure this was part of the outputs list in AM_mcmc_parameters.\n", sep="");
-			return (NULL);
-		}
-		
-		if(is.null(df)) {
-			df = tmp;
-		} else {
-			if (nrow(df) != nrow(tmp)) {
-				cat("ERROR: Invalid extraction size, previously found ",nrow(df),"while with target '",target,"' we have ", nrow(tmp),"\n", sep="");
-				return (NULL);
-			}
-			df = data.frame(df,tmp);
-		}
-	}
-	return (df);
 }
