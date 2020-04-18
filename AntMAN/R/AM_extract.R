8565f9624514b24d58c1555c0b94720f8318e8d8
Bruno <bruno.bodin@yale-nus.edu.sg>
2020-04-18 21:12:37 +0800

    Add missing part of the AM_extract file

--- AntMAN/R/AM_extract.R
+++ AntMAN/R/AM_extract.R
@@ -134,3 +134,58 @@ extract_target = function(fit, target, iterations = NULL, debug = FALSE){
 	}
 	return (result)
 }
+
+
+
+#'  Extract values within a AM_mcmc_output object. 
+#' 
+#'  Due to the complexity of AntMAN outputs, AM_mcmc_output object can be difficult
+#'  to handle. The AM_extract function ease access of particular variables within 
+#'  AM_mcmc_output object.
+#'  
+#'@param object a \code{\link{AM_mcmc_output}} object
+#'@param targets List of variables to extract (ie. K, M, Mna, Tau_mu).
+#'@param iterations Can specify particular iterations to extracts, NULL for all.
+#'  
+#'@export
+AM_extract = function(object, targets, iterations = NULL, debug = FALSE){
+	
+	df = NULL;
+	for (target in targets) {
+		
+		
+		if (target == "CI") {
+			## CI Extractor
+			nrows = length(object$CI)
+			ncols = length(object$CI[[1]])
+			tmp = data.frame(array(as.numeric(unlist(object$CI)), dim=c(nrows, ncols)))
+			names(tmp) <- generate_column_names(target,c(1:ncols));
+			if (!is.null(iterations)) {
+				tmp = tmp[iterations,];
+			}
+		} else {
+			## Generic extractor (SLOW)
+			tmp = extract_target(object,target,iterations,debug);
+		}
+		
+		
+		if (is.null(tmp)) {
+			warning("ERROR: Invalid extraction target: ",target,", please make sure this was part of the outputs list in AM_mcmc_parameters.\n", sep="");
+			return (NULL);
+		}
+		
+		if(is.null(df)) {
+			df = tmp;
+		} else {
+			if (nrow(df) != nrow(tmp)) {
+				warning("ERROR: Invalid extraction size, previously found ",nrow(df),"while with target '",target,"' we have ", nrow(tmp),"\n", sep="");
+				return (NULL);
+			}
+			df = data.frame(df,tmp);
+		}
+	}
+	return (df);
+	
+	
+	
+}
