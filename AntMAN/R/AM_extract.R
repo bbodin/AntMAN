d10a8b09b041cdd898fcb8dc6ad158a53c3e94d0
Priscilla <prisong97@yahoo.com.sg>
2021-02-01 13:02:16 +0800

    modified documentation of AM_extract

--- AntMAN/R/AM_extract.R
+++ AntMAN/R/AM_extract.R
@@ -158,12 +158,15 @@ extract_target = function(fit, target, iterations = NULL, debug = FALSE){
 
 #'  Extract values within a AM_mcmc_output object. 
 #' 
+#'  Given an object produced by AM_mcmc_fit, as well as the target variable names, AM_extract will return a list of the variables of interest.
 #'  Due to the complexity of AntMAN outputs, AM_mcmc_output object can be difficult
-#'  to handle. The AM_extract function ease access of particular variables within 
-#'  AM_mcmc_output object.
+#'  to handle. The AM_extract function eases access of particular variables within the
+#'  AM_mcmc_output object. Variables of varying dimension are expected to result from the transdimensional moves. When considering such
+#'  variables, the extracted list would correspond to an nx1 list, where n refers to the number of extracted iterations. Each of these nx1 entries consists
+#'  of another list of dimension mx1, where m specifies the number of components inferred for that iteration.
 #'  
 #'@param object a \code{\link{AM_mcmc_output}} object
-#'@param targets List of variables to extract (ie. K, M, Mna, mu).
+#'@param targets List of variables to extract (ie. K, M, mu).
 #'@param iterations Can specify particular iterations to extracts, NULL for all.
 #'@param debug Activate log to 
 #'  
