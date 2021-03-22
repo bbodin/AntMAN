e2c7188e87c1085fe6ed59f2dce856c9d0c43b00
Priscilla <prisong97@yahoo.com.sg>
2021-03-22 20:00:47 +0800

    changed tests to suit modifications

--- AntMAN/tests/UnitTest_AM_binder.R
+++ AntMAN/tests/UnitTest_AM_binder.R
@@ -22,11 +22,12 @@ res = AM_demo_uvn_poi ()
 ### Run
 ##############################################
 
-binder_result = AM_salso(res$fit, with_coclustering_probability=TRUE)
+binder_result = AM_binder(res$fit)
+cluster = binder_result[["Labels"]]
+coclustering_probability = AM_coclustering(res$fit)
 #binder_result = AM_binder(res$fit , with_coclustering_probability=TRUE)
-
-cluster                    = binder_result[["clustering"]]
-coclustering_probability   = binder_result[["coclustering_probability"]]
+# cluster                    = binder_result[["clustering"]]
+# coclustering_probability   = binder_result[["coclustering_probability"]]
 
 ##############################################
 ### Verify
